298523b720a273573a1611463ae55d3061fe3f91
Hitansh Jain <hitanshjain77@gmail.com>
2026-07-12 15:15:03 +0530

    feat: setup Google Analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -120,6 +120,18 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en" className="dark">
       <head>
         <HeadContent />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PGKEQZ5RCW" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PGKEQZ5RCW');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
